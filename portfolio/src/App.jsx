738cad19fc007202d7792f4dbba5884d5e476cdc
Steve Calla <callasteven@gmail.com>
2024-02-12 14:17:31 -0700

    Adjust project json app jsx and vite config

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap"; // Import all of Bootstrap's JS
